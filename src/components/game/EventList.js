c980ff7e43f9d4fd2f0fab3a0df88529a8c9d5d8
Ryan Beiden <rdbeiden@gmail.com>
2020-11-30 21:27:48 -0600

    leave event

--- src/components/game/EventList.js
+++ src/components/game/EventList.js
@@ -3,7 +3,7 @@ import { EventContext } from "./EventProvider.js"
 import "./Events.css"
 
 export const EventList = (props) => {
-    const { events, getEvents, joinEvent } = useContext(EventContext)
+    const { events, getEvents, joinEvent, leaveEvent } = useContext(EventContext)
 
     useEffect(() => {
         getEvents()
@@ -28,9 +28,15 @@ export const EventList = (props) => {
                         <div>
                             {event.date} @ {event.time}
                         </div>
-                        <button className="btn btn-2"
-                                onClick={() => joinEvent(event.id)}
-                        >Join</button>
+                        {
+                            event.joined
+                                ? <button className="btn btn-3"
+                                    onClick={() => leaveEvent(event.id)}
+                                    >Leave</button>
+                                : <button className="btn btn-2"
+                                    onClick={() => joinEvent(event.id)}
+                                    >Join</button>
+                        }
                     </section>
                 })
             }
